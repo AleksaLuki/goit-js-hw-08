63d9e064a6c01400954fe5b6ce618e059daff0d4
AleksaLuki <dimirtiy9000@gmail.com>
2023-03-06 13:44:24 +0200

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,55 @@
 import throttle from 'lodash.throttle';
 
 
-form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+ 
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputData, 500));
+
+    populateTextarea();
+
+
+function onInputData() {
+  const data = {email: email.value, 
+    message: message.value,};
+
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(data));
+}
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(localStorage.getItem(LOCAL_KEY));
+
+  localStorage.removeItem(LOCAL_KEY);
+}
+ 
+
+function populateTextarea() {
+  const saveData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+
+    if (saveData === null) {
+      return;
+    }
+
+    email.value = saveData.email;
+    message.value = saveData.message;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
